774272bcef245453bbb9424321aa73b07e57e60a
leontristain <ltristain@yahoo.com>
2020-05-07 00:03:32 -0700

    implementation for GenericPackageSource

--- skypackages/sources.py
+++ skypackages/sources.py
@@ -4,12 +4,61 @@ from skypackages.utils import yaml_dump, yaml_load
 
 
 @dataclass
+class GenericPackageSource:
+    file_name: str
+    size: int
+    url: str
+    notes: str
+
+    def __repr__(self):
+        return f'GenericPackageSource({self.file_name} {self.size}'
+
+    def validate(self, file_path):
+        assert file_path.name == self.file_name
+        size = int(file_path.stat().st_size / 1024)
+        assert size == self.size, (
+            f'file sizes do not match for {file_path}, got {size}, expected '
+            f'{self.size}')
+
+    def match_entry(self, entry):
+        return (
+            entry['class'] == self.__class__.__name and
+            entry['file_name'] == self.file_name and
+            entry['size'] == self.size)
+
+    @property
+    def entry(self):
+        return {**{'class': self.__class__.__name__}, **asdict(self)}
+
+    @classmethod
+    def from_entry(cls, entry):
+        return cls(**entry)
+
+    def save_details(self, blob_id, sources_folder):
+        sources_file = sources_folder / f'{blob_id}.yaml'
+        sources = {}
+        if sources_file.exists():
+            sources = yaml_load(sources_file.read_text())
+
+        entries = sources.setdefault('entries', [])
+        for entry in entries:
+            if self.match(entry):
+                entry['url'] = self.url
+                entry['notes'] = self.notes
+                break
+        else:
+            entries.append(self.entry)
+
+        sources_file.write_text(yaml_dump(sources))
+
+
+@dataclass
 class NexusPackageSource:
     game: str
     mod_id: int
     file_id: int
     file_name: str
-    size: str
+    size: int
 
     def __repr__(self):
         return (
